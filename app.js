52e85506ca14d4f52eb3cf5f61ee2d1fd536a675
David Lemvigh <dlemvigh@gmail.com>
2015-06-30 11:27:08 +0200

    meteor angular socially tutorial

---
+++ app.js
@@ -0,0 +1,38 @@
+Parties = new Mongo.Collection("parties");
+
+if (Meteor.isClient) {
+
+  angular.module('socially',['angular-meteor']);
+
+  angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor',
+    function($scope, $meteor){
+
+      $scope.parties = $meteor.collection(Parties);
+
+      $scope.remove = function(party){
+        $scope.parties.splice( $scope.parties.indexOf(party), 1 );
+      };
+
+      $scope.removeAll = function(){
+        $scope.parties.remove();
+      };
+
+    }]);
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Parties.find().count() === 0) {
+      var parties = [
+        {'name': 'Dubstep-Free Zone',
+          'description': 'Fast just got faster with Nexus S.'},
+        {'name': 'All dubstep all the time',
+          'description': 'Get it on!'},
+        {'name': 'Savage lounging',
+          'description': 'Leisure suit required. And only fiercest manners.'}
+      ];
+      for (var i = 0; i < parties.length; i++)
+        Parties.insert(parties[i]);
+    }
+  });
+}

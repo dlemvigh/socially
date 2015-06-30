40051e1ed98c331cfa8aee5698edc83a50d79c9f
David Lemvigh <dlemvigh@gmail.com>
2015-06-30 23:53:18 +0200

    lots of angular meteor stuff

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,1 +1,10 @@
-angular.module('socially',['angular-meteor', 'ui.router']);
+angular.module('socially',['angular-meteor', 'ui.router', 'angularUtils.directives.dirPagination']);
+
+function onReady() {
+  angular.bootstrap(document, ['socially']);
+}
+
+if (Meteor.isCordova)
+  angular.element(document).on("deviceready", onReady);
+else
+  angular.element(document).ready(onReady);
--- client/parties/controllers/paryDetails.js
+++ client/parties/controllers/paryDetails.js
@@ -1,12 +1,34 @@
 angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
-	function($scope, $stateParams, $meteor){
-		$scope.party = $meteor.object(Parties, $stateParams.partyId, false);
-		
-		$scope.save = function(){
-			$scope.party.save();
-		};
-		
-		$scope.reset = function(){
-			$scope.party.reset();
-		}
-}]);
+  function($scope, $stateParams, $meteor){
+
+    $scope.party = $meteor.object(Parties, $stateParams.partyId);
+
+    var subscriptionHandle;
+    $meteor.subscribe('parties').then(function(handle) {
+      subscriptionHandle = handle;
+    });
+
+    $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
+
+    $scope.canInvite = function(){
+      if (!$scope.party)
+        return false;
+
+      return !$scope.party.public && $scope.party.owner === Meteor.userId();
+    }
+
+    $scope.invite = function(user){
+      $meteor.call('invite', $scope.party._id, user._id).then(
+        function(data){
+          console.log('success inviting', data);
+        },
+        function(err){
+          console.log('failed', err);
+        }
+      );
+    };
+
+    $scope.$on('$destroy', function() {
+      subscriptionHandle.stop();
+    });
+  }]);

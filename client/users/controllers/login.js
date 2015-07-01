d6c3fc79d6443da880d2d33481aa0cb87a79b719
David Lemvigh <dlemvigh@gmail.com>
2015-07-01 22:41:01 +0200

    step 18 missing

---
+++ client/users/controllers/login.js
@@ -0,0 +1,25 @@
+angular.module("socially").controller("LoginCtrl", ['$meteor', '$state',
+  function($meteor, $state){
+    var vm = this;
+
+    vm.credentials = {
+      email: '',
+      password: ''
+    };
+
+    vm.error = '';
+
+    vm.login = function (){
+      $meteor.loginWithPassword(vm.credentials.email, vm.credentials.password).then(
+        function(){
+          $state.go('parties');
+        },
+        function(err){
+          vm.error = 'Login error - ' + err;
+        }
+      );
+    };
+  }
+]);/**
+ * Created by dale on 01-07-2015.
+ */
